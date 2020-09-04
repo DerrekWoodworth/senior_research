e2b3923999ebd421eeb54f34f738403f3563dd45
Derrek Woodworth <derrek.woodworth@gmail.com>
2020-09-04 14:32:01 +0000

    Modified scripts to be run from any directory and added proto build script to launch script

--- proto/generate_code_from_proto.sh
+++ proto/generate_code_from_proto.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # This file handles running the protoc codegen in a docker container and putting the resulted code in the frontned and backend
+realname=$(realpath $0)
+
 
 # Build docker image to run the protoc in
-docker build -t protoc-backend -f Dockerfile.backend .
+docker build -t protoc-backend -f $(dirname $realname)/Dockerfile.backend $(dirname $realname)
 
-docker run --rm  -v $PWD/../:/root/senior_research protoc-backend /bin/bash -c "cd /root/senior_research/proto; ./codegen_backend.sh"
+docker run --rm  -v $(dirname $(dirname $realname)):/root/senior_research protoc-backend /bin/bash -c "cd /root/senior_research/proto; ./codegen_backend.sh"
 
-docker build -t protoc-frontend -f Dockerfile.frontend .
+docker build -t protoc-frontend -f $(dirname $realname)/Dockerfile.frontend $(dirname $realname)
 # Do the same thing with node for the frontend
-docker run --rm  -v $PWD/../:/root/senior_research protoc-frontend /bin/bash -c "cd /root/senior_research/proto; ./codegen_frontend.sh"
+docker run --rm  -v $(dirname $(dirname $realname)):/root/senior_research protoc-frontend /bin/bash -c "cd /root/senior_research/proto; ./codegen_frontend.sh"
--- start_service.sh
+++ start_service.sh
@@ -4,12 +4,19 @@
 # Set up the ~pid~ files
 mkdir -p ./running
 
+# Generate the proto code
+./proto/generate_code_from_proto.sh
+
 
 ./backend/run_backend.sh
 ./frontend/run_frontend.sh
 ./envoy/run_envoy.sh
 
-echo Reach the UI at http://localhost:4200
-
 echo Started the frontend, backend, and proxy
+
+echo The services take a some time to boot, watch the logs until angular comes up
+echo Run this command until you see -- Angular Live Development Server is listening on 0.0.0.0:4200, open your browser on http://localhost:4200/ 
+echo docker logs -f $(cat $(dirname $0)/running/frontend)
+
+
 
