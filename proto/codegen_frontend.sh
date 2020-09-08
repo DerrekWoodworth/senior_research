61b69f6092647ae746118544afa0ee39e5cce05d
Derrek Woodworth <derrek.woodworth@gmail.com>
2020-09-08 17:32:25 +0000

    Changing codegen to handle multiple proto files

--- proto/codegen_frontend.sh
+++ proto/codegen_frontend.sh
@@ -4,4 +4,4 @@
 
 echo Building frontend using protoc...
 # Frontend codegen
-bash -c 'cd ../frontend; npm install; protoc --plugin=protoc-gen-ts=node_modules/.bin/protoc-gen-ts --js_out="import_style=commonjs,binary:src/app/generated" --ts_out="service=grpc-web:src/app/generated" --proto_path ../proto/proto/ ../proto/proto/container.proto'
+bash -c 'cd ../frontend; npm install; protoc --plugin=protoc-gen-ts=node_modules/.bin/protoc-gen-ts --js_out="import_style=commonjs,binary:src/app/generated" --ts_out="service=grpc-web:src/app/generated" --proto_path ../proto/proto/ ../proto/proto/*'
